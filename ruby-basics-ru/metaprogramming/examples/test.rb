aac4ceb5354e643e872d3e699a05fc232a263af5
@hexlet/cli <support@hexlet.io>
2024-02-25 20:04:31 +0400

    submit ruby-basics-ru/metaprogramming

---
+++ ruby-basics-ru/metaprogramming/examples/test.rb
@@ -0,0 +1,4 @@
+require_relative '../lib/model'
+require_relative '../lib/post'
+
+post = Post.new title: 'Hello, World!', body: 'First Post'
---
+++ ruby-basics-ru/metaprogramming/lib/model.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+# BEGIN
+require 'date'
+
+module Model
+  attr_reader :attributes
+
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  def initialize(params = {})
+    @attributes = {}
+
+    self.class.attribute_schema.each do |name, options|
+      @attributes[name] = set_type(params[name], options[:type])
+    end
+  end
+
+  def set_type(value, type)
+    return value if value.nil?
+    return DateTime.parse(value) if type == :datetime
+
+    value
+  end
+
+  module ClassMethods
+    attr_reader :attribute_schema
+
+    def attribute(name, options = {})
+      @attribute_schema ||= {}
+      @attribute_schema[name] = options
+
+      define_method :"#{name}" do
+        @attributes[name]
+      end
+
+      define_method :"#{name}=" do |value|
+        puts 'called'
+        @attributes[name] = set_type(value, options[:type])
+      end
+    end
+  end
+end
+# END
